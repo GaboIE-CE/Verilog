142a3575cc7574b1d233ab38b25070c1ef2bdefb
Gabo <gabrielvegachavez12@gmail.com>
2025-05-03 10:05:12 -0600

    v2

--- digitales_2/Tarea_3/maquina_estados.v
+++ digitales_2/Tarea_3/maquina_estados.v
@@ -9,11 +9,13 @@ module cajero(
     input DIGITO_STB,
     input [3:0]DIGITO,
     input  [15:0]PIN_CORRECTO ,
+    input [31:0]MONTO,
     input MONTO_STB,
-    input BALANCE_INICIAL,    
+    input [63:0]BALANCE_INICIAL,
+    input TIPO_TRANS,    
   
     output reg BALANCE_STB,     
-    output reg BALANCE_ACTUALIZADO,
+    output reg [63:0] BALANCE_ACTUALIZADO,
     output reg ENTREGAR_DINERO,
     output reg PIN_INCORRECTO,
     output reg ADVERTENCIA,
@@ -26,32 +28,80 @@ module cajero(
 
 reg [3:0]contador_DIGITO = 0; 
 reg [3:0]contador_DIGITO_next = 0; // Con este contamos por cual parte del PIN vamos
-reg [1:0] estado_actual, estado_siguiente;
+reg [9:0]contador_balance_stb_siguiente = 0;
+reg [9:0]contador_balance_stb = 0;
+reg [1:0] estado_actual = 0;
+reg [1:0]estado_siguiente = 0 ;
+reg DIGITO_STB_ANTERIOR = 0;
+reg [63:0] BALANCE_ACTUALIZADO_siguiente = 0;
+reg bandera_fondos_insuficientes = 0;
+reg BALANCE_STB_siguiente=0;
+reg ENTREGAR_DINERO_siguiente = 0;
+
 // Estados
 parameter IDLE = 2'b00;
 parameter INICIO = 2'b01;
+parameter TRANSACCION = 2'b10;
+parameter DINERO = 2'b11;
 
 always @(posedge clock) begin
     estado_actual <= estado_siguiente;
     contador_DIGITO <= contador_DIGITO_next;
+    DIGITO_STB_ANTERIOR <= DIGITO_STB;
+    BALANCE_ACTUALIZADO <= BALANCE_ACTUALIZADO_siguiente;
+    FONDOS_INSUFICIENTES <= bandera_fondos_insuficientes;
+    BALANCE_STB <= BALANCE_STB_siguiente;
+    ENTREGAR_DINERO <= ENTREGAR_DINERO_siguiente
     
 end
 
 always @(*) begin
-    if(TARJETA_RECIBIDA)begin
-        estado_siguiente = INICIO;
-    end
+    estado_siguiente = estado_actual;
+    contador_DIGITO_next = contador_DIGITO;
+    BALANCE_STB_siguiente = 0; // por defecto
+
+
+
+   if (estado_actual == IDLE) begin
+     if(TARJETA_RECIBIDA && contador_DIGITO_next == 0)begin
+         estado_siguiente = INICIO;
+    end    
+   end 
+
 
     if(estado_actual == INICIO) begin
-        if(DIGITO_STB == 1)begin
-            $display(PIN_CORRECTO[(contador_DIGITO*4) +:4]);
-            $display(PIN_CORRECTO);
-            if(DIGITO == PIN_CORRECTO[(contador_DIGITO*4) +:4])begin
-                $display("El digito corresponde al pint");
+        if(DIGITO_STB == 1 && contador_DIGITO <4 && DIGITO_STB_ANTERIOR == 0)begin
+            //$display("PIN[%0d] = %d", contador_DIGITO, PIN_CORRECTO[15 - contador_DIGITO*4 -: 4]); // Toma 4 bits empezando en el bit más significativo (bit 15), y avanza hacia abajo en grupos de 4 bits
+            //$display(DIGITO);
+            if(DIGITO == PIN_CORRECTO[15 - contador_DIGITO*4 -: 4])begin
+                //$display("El digito corresponde al pint");
                 contador_DIGITO_next = contador_DIGITO + 1;
-            end
+            end //else if(DIGITO != PIN_CORRECTO[15 - contador_DIGITO*4 -: 4])
+        end else if(contador_DIGITO == 4) begin       
+                    $display("Pin correcto ingresado");
+                    estado_siguiente = TRANSACCION;
+                    $display(estado_siguiente);
+
         end
     end 
+    if(estado_actual == TRANSACCION) begin
+        if(MONTO<BALANCE_INICIAL)begin
+            BALANCE_ACTUALIZADO_siguiente = BALANCE_INICIAL - MONTO;
+            BALANCE_STB_siguiente = 1;
+            estado_siguiente = DINERO;
+            $display("Balance actualizado");
+            $display(BALANCE_ACTUALIZADO_siguiente);
+
+        end else begin
+            $display("Fondos insuficientes");
+            bandera_fondos_insuficientes = 1;
+        end
+
+    end
+    if(estado_actual == DINERO) begin
+        ENTREGAR_DINERO_siguiente = 1;
+        
+    end
 end
 
 endmodule
--- digitales_2/Tarea_3/tester.v
+++ digitales_2/Tarea_3/tester.v
@@ -10,11 +10,13 @@ module probador(
     output reg DIGITO_STB,
     output reg [3:0]DIGITO,
     output reg  [15:0]PIN_CORRECTO,
+    output reg [31:0] MONTO,
     output reg MONTO_STB,
-    output reg BALANCE_INICIAL,    
+    output reg [63:0]BALANCE_INICIAL, 
+    output reg TIPO_TRANS,   
   
     input BALANCE_STB,     
-    input BALANCE_ACTUALIZADO,
+    input [63:0] BALANCE_ACTUALIZADO,
     input ENTREGAR_DINERO,
     input PIN_INCORRECTO,
     input ADVERTENCIA,
@@ -31,18 +33,45 @@ end
 
 initial begin
     clock = 0;
+    MONTO_STB = 0;
 
 
-    PIN_CORRECTO = 16'b0010_0000_1001_1000;
+    PIN_CORRECTO = 16'b1000_0011_0100_0100; // = 8 3 4 4
+    TIPO_TRANS   = 1;
     TARJETA_RECIBIDA = 1;
+    BALANCE_INICIAL  = 64'b0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0110_0100;
+    
+
+
     DIGITO_STB = 0;
     #10;
-    DIGITO = 4'b0010;
-    #10;
+    DIGITO = 4'b1000;
     DIGITO_STB = 1;
     #10;
     DIGITO_STB = 0;    
-    #20
+    #10
+    DIGITO = 4'b0011;
+    DIGITO_STB = 1;
+    #10;
+    DIGITO_STB = 0;
+    #10
+    DIGITO = 4'b0100;
+    DIGITO_STB = 1;
+    #10;
+    DIGITO_STB = 0;
+    #10
+    DIGITO = 4'b0100;
+    DIGITO_STB = 1;
+    #10;
+    DIGITO_STB = 0;   
+    MONTO = 32'b0000_0000_0000_0000_0000_0000_0011_0010;
+    MONTO_STB = 1;
+    #10
+    MONTO_STB = 0;
+
+    #100
+
+
     $finish;
     
 end
